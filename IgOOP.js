f207e145b79ad95ec2cbab8a3f57af1615de91b8
Igcalinos-Christian <igcalchristian@gmail.com>
2024-09-08 15:26:55 +0800

    Revised OOP assignment.

--- IgOOP.js
+++ IgOOP.js
@@ -1,11 +1,15 @@
-let sum;
-class Summy{
-    Add(a, b){
-        sum = a + b;
-        return sum;
+class MyClass{
+    constructor(a, b){
+        this.x = Number(a);
+        this.y = Number(b); 
+    }
+    sum(){
+        let c = this.x;
+        let d = this. y;
+        let SUM = c + d;
+        return SUM;
     }
 }
 
-const Sum = new Summy();
-const total = Sum.Add(1,2);
-console.log(total);
+const Sum = new MyClass(2, 4);
+console.log(Sum.sum());
