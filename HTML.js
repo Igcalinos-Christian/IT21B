b039d278e81e7e7eedb30de9422be4217b0b85e2
Igcalinos-Christian <igcalchristian@gmail.com>
2024-09-11 10:24:07 +0800

    OOP act

---
+++ HTML.js
@@ -0,0 +1,35 @@
+let IsFragrant = true;
+
+class Flower{
+    constructor (Name, Season, Color){
+        this.name = Name;
+        this.color = Color;
+        this.season = Season;
+    }
+    NAME(){
+        console.log(`The ${this.name} blooms in ${this.season}.`);
+        document.getElementById("output").innerHTML +=`The ${this.name} blooms in ${this.season}.<br>`;
+    }
+    
+    isFragrant(){
+        if(IsFragrant = true){
+            console.log(`The ${this.name} is fragrant.`);
+            document.getElementById("output").innerHTML +=`The ${this.name} is fragrant.<br>`;
+        }else if(IsFragrant = false){
+            console.log(`The ${this.name} is not fragrant.`);
+            document.getElementById("output").innerHTML +=`The ${this.name} is not fragrant.<br>`;
+        }
+    }
+
+    COLOR(){
+        console.log(`The ${this.name} a beautiful ${this.color} color.`);
+        document.getElementById("output").innerHTML +=`The ${this.name} a beautiful ${this.color} color.<br>`;
+    }
+
+}
+
+const FLOWER = new Flower(Rose, Spring, Red);
+FLOWER.NAME();
+FLOWER.isFragrant();
+FLOWER.COLOR();
+
